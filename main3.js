d5f79d453e7677df8d9425279b490c5659fc5230
ANNASTASIEVNA <90769228+ANNASTASIEVNA@users.noreply.github.com>
2022-04-10 20:06:25 +0300

    lesson3
    ls3

---
+++ main3.js
@@ -0,0 +1,37 @@
+// for (let i = 0; i <=10; i++) {
+//     if (i === 0){
+//         console.log(i, "ноль");
+//     } else if (i % 2 ===0){
+//         console.log(i, "четное число");
+//     } else {
+//         console.log(i, "нечетное число");
+//     }
+    
+// }
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// arr.splice(3, 2);
+// console.log(arr);
+
+
+// const arr = [];
+// for (let i = 0; i < 5; i++) {
+//   arr.push(Math.round(Math.random() * 10));
+// }
+// console.log(arr)
+// console.log(arr.map(item => item ** 2));
+
+
+// const arr = [];
+//     const arrFilter = [];
+//     for (let i = 0; i < 5; i++) {
+//       arr.push(Math.round(Math.random() * 10));
+//     }
+
+//     console.log(arr.filter(item => item !== 3));
+//     console.log(arr.map(elem => elem += elem * 0.1));
+//     console.log(arr);
+
+// for(var number = 0; number <= 9; document.write(number++ + " ")){
+
+// }
